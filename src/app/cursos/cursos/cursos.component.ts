73401bf8f5d4bf535209b97cc0039bd4b0aba909
guifroes1984 <guilhermefroes26@gmail.com>
2023-07-25 00:52:28 -0300

    Lista de Cursos: Tratamento de Erros e MatDialog

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,26 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+
 import { Curso } from '../model/curso';
 import { CursosService } from '../services/cursos.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.scss']
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent {
 
   cursos$: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
-  constructor(private cursoService: CursosService) {
+  constructor(
+    private cursoService: CursosService,
+    public dialog: MatDialog) {
 
-    this.cursos$ = this.cursoService.list();
+    this.cursos$ = this.cursoService.list()
+    .pipe(
+      catchError(error => {
+        this.onError('Erro ao carregar cursos.')
+        return of([])
+      })
+    );
 
   }
 
-  ngOnInit(): void {
-
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
   }
-
 }
+
